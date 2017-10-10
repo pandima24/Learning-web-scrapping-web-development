3ced6b197e101eff327749d0a4b52684d9b7bbf7
pandima24 <pandima.chandran@gmail.com>
2017-10-10 18:58:48 +0530

    added few states to array

--- index.py
+++ index.py
@@ -1,11 +1,53 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-page = urllib2.urlopen('http://workerscompensation.com/').read()
-soup = BeautifulSoup(page)
-soup.prettify()
-for anchor in soup.findAll('a', href=True):
-    stateLink = anchor['href']
-    if '../' in stateLink:
-        print stateLink
+# page = urllib2.urlopen('http://workerscompensation.com/').read()
+# soup = BeautifulSoup(page)
+# soup.prettify()
+# for anchor in soup.findAll('a', href=True):
+#     stateLink = anchor['href']
+#     if '../' in stateLink:
+#         print 'http://workerscompensation.com/' + stateLink.replace('../','')
 
+states = [
+    'alabama',
+    'alaska',
+    'arizona',
+    'arkansas',
+    'california',
+    'colorado',
+    'connecticut',
+    'delaware',
+    'florida',
+    'georgia',
+    'hawaii',
+    'idaho',
+    'illinois',
+    'indiana',
+    'iowa',
+    'kansas',
+    'kentucky',
+    'louisiana',
+    'maine',
+    'maryland',
+    'massachusetts',
+    'michigan',
+    'minnesota',
+    'mississippi',
+    'missouri',
+    'montana',
+    'nebraska',
+    'nevada',
+    'new_hampshire',
+    'new_jersey',
+    'new_mexico',
+    'new_york',
+    'north_carolina',
+    'north_dakota',
+    'ohio',
+    'oklahoma',
+    'oregon',
+    'pennsylvania',
+    'rhode_island',
+
+]

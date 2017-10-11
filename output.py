3174ce642b6eaafaa8e51cc886278ed6285ce00e
siva <gnusiva@gmail.com>
2017-10-12 00:25:41 +0530

    worked on recursive funtion but not yet done as required

--- output.py
+++ output.py
@@ -10,14 +10,22 @@ def writeToFile( state, title, url ):
 def recursiveParse(title,url):
     html = webPage.open(url)
     arr2 = title
+    firstTimeEntry = False
     for li in html.find_all('li'):
         atag = li.find('a')
         if '?ID=' in atag['href']:
             if 'stateitem.php' not in atag['href']:
+                if firstTimeEntry:
+                    del arr2[-1]
+                firstTimeEntry = True
                 arr2.append( atag.string )
                 recursiveParse( arr2, 'http://www.workerscompensation.com/regulations/statedepartment.php'+atag['href'] )
             else:
                 arr2.append( atag.string )
                 print atag['href']
-                print arr2
+                for item in arr2:
+                    print item
+                del arr2[-1]
+                print '============================='
+            #del arr2[-1]
     #print returnArr2
